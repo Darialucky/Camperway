04ef8c0b905c29a206cb68e26d2e5f9c30dc7c35
Darialucky <144655337+Darialucky@users.noreply.github.com>
2024-08-18 15:36:40 +0300

    Calendar

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import css from "./RegistrationForm.module.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { addDays } from "date-fns";
 
 const RegistrationForm = () => {
   const [formData, setFormData] = useState({
@@ -32,23 +33,46 @@ const RegistrationForm = () => {
     return Object.keys(newErrors).length === 0;
   };
 
-  const handleChange = (date) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: "",
+    }));
+  };
+
+  const handleDateChange = (date) => {
     setFormData({
       ...formData,
       bookingDate: date,
     });
+
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      bookingDate: "",
+    }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (validate()) {
-      console.log("Form is valid");
       alert("Form submitted successfully!");
-      setTimeout(() => {
-        window.location.reload();
-      }, 2000);
+      setFormData({
+        name: "",
+        email: "",
+        bookingDate: null,
+        comment: "",
+      });
     }
   };
+
+  const today = new Date();
 
   return (
     <form className={css.formContainer} onSubmit={handleSubmit}>
@@ -57,65 +81,76 @@ const RegistrationForm = () => {
         Stay connected! We are always ready to help you.
       </p>
       <div className={css.formInput}>
-        <input
-          className={`${css.inputField} ${css.nameField}`}
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={formData.name}
-          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-        />
-        {errors.name && <span className={css.error}>{errors.name}</span>}
-
-        <input
-          className={`${css.inputField} ${css.emailField}`}
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-        />
-        {errors.email && <span className={css.error}>{errors.email}</span>}
-
-        <div className={css.dateWrapper}>
-          <DatePicker
-            id="bookingDate"
-            className={css.dateField}
-            selected={formData.bookingDate}
+        <div className={css.inputWrapper}>
+          <input
+            className={`${css.inputField} ${css.nameField} ${
+              errors.name ? css.errorBorder : ""
+            }`}
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={formData.name}
             onChange={handleChange}
-            placeholderText="Booking date"
-            dateFormat="dd/MM/yyyy"
           />
-          <span className={css.calendarIcon}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M16 2v2M8 2v2M4 6h16M20 10H4v10h16V10z"
-              ></path>
-            </svg>
-          </span>
+          {errors.name && <div className={css.tooltip}>{errors.name}</div>}
         </div>
-        {errors.bookingDate && (
-          <span className={css.error}>{errors.bookingDate}</span>
-        )}
+
+        <div className={css.inputWrapper}>
+          <input
+            className={`${css.inputField} ${css.emailField} ${
+              errors.email ? css.errorBorder : ""
+            }`}
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          {errors.email && <div className={css.tooltip}>{errors.email}</div>}
+        </div>
+
+        <div className={css.inputWrapper}>
+          <div className={css.dateWrapper}>
+            <DatePicker
+              id="bookingDate"
+              className={`${css.dateField} ${
+                errors.bookingDate ? css.errorBorder : ""
+              }`}
+              selected={formData.bookingDate}
+              onChange={handleDateChange}
+              placeholderText="Booking date"
+              dateFormat="dd/MM/yyyy"
+              minDate={today}
+            />
+            <span className={css.calendarIcon}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16 2v2M8 2v2M4 6h16M20 10H4v10h16V10z"
+                ></path>
+              </svg>
+            </span>
+          </div>
+          {errors.bookingDate && (
+            <div className={css.tooltip}>{errors.bookingDate}</div>
+          )}
+        </div>
 
         <textarea
           className={`${css.inputField} ${css.commentField}`}
           name="comment"
           placeholder="Comment"
           value={formData.comment}
-          onChange={(e) =>
-            setFormData({ ...formData, comment: e.target.value })
-          }
+          onChange={handleChange}
         ></textarea>
       </div>
       <button type="submit" className={css.submitButton}>
