7e2e164776ce2e24c56e40412ae9d17613289071
Darialucky <144655337+Darialucky@users.noreply.github.com>
2024-09-22 22:28:53 +0300

    css ikon location

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -3,7 +3,12 @@ import sprite from "../assets/sprite.svg";
 
 const Icon = ({ name, className, width = 24, height = 24 }) => {
   return (
-    <svg className={className} width={width} height={height}>
+    <svg
+      className={className}
+      width={width}
+      height={height}
+      stroke="currentColor"
+    >
       <use xlinkHref={`${sprite}#${name}`} />
     </svg>
   );
