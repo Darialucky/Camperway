524a4d4723b48275c72538737ad07e96d9b8f5f5
Darialucky <144655337+Darialucky@users.noreply.github.com>
2024-09-23 22:41:02 +0300

    зміни в стилях локації

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -3,12 +3,7 @@ import sprite from "../assets/sprite.svg";
 
 const Icon = ({ name, className, width = 24, height = 24 }) => {
   return (
-    <svg
-      className={className}
-      width={width}
-      height={height}
-      stroke="currentColor"
-    >
+    <svg className={className} width={width} height={height}>
       <use xlinkHref={`${sprite}#${name}`} />
     </svg>
   );
